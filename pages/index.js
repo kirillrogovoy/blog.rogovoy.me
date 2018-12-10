b2f82cc66d23941b047d3a6179b903251c6b2d38
Kirill Rogovoy <kirill@rogovoy.me>
2018-12-10 19:01:59 +0200

    fixes

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
 import Head from 'next/head'
 import Layout from '../components/layout'
 import PostList from '../components/post-list'
-import { posts } from '../post'
+import { getAllPosts } from '../post'
 
 export default () => <div>
   <Layout>
     <Head>
       <title>Кирилл Роговой – блог</title>
     </Head>
-    <PostList posts={posts} />
+    <PostList posts={getAllPosts()} />
   </Layout>
 </div>
